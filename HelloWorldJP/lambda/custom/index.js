738ef7f1bce335d51553b7babe65d6ce06ccc88e
Kuniaki Shimizu <kun432.8d1w@gmail.com>
2018-06-02 22:11:41 +0900

    add ja-JP

---
+++ HelloWorldJP/lambda/custom/index.js
@@ -0,0 +1,104 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'アレクサスキルキットへようこそ！ハローワールド！';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('Hello World', speechText)
+      .getResponse();
+  },
+};
+
+const HelloWorldIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'ハローワールド';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('ハローワールド', speechText)
+      .getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'ハローワールドと言ってみてください';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('ハローワールド', speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'さようなら';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('ハローワールド', speechText)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('すいません、なんと言っているのか理解できませんでした。もう一度言ってみてください。')
+      .reprompt('すいません、なんと言っているのか理解できませんでした。もう一度言ってみてください。')
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    HelloWorldIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
--- README.md
+++ README.md
@@ -1,33 +1,18 @@
 # alexa-ask-cli-sample
 
-# Usage
+## Prerequisite
 
-1. Setting IAM role for ask-cli
+- AWS account
+- Amazon Developer account
+- ask-cli installed and initialized
 
-  - Login to AWS
-  - Choose IAM
-  - Create New Group
-    - Name group
-      - ex: "ask-cli-group"
-    - Choose following policy
-      - AWSLambdaFullAccess
-      - IAMFullAccess
-  - Confirm to create group
-  - Create User
-    - Name user
-      - ex: "ask-cli-user"
-    - Choose AWS access type
-      - "access by program"
-    - Join user to group which created above
-    - confirm to create user
-  - Download Access Key and Secret Access Key
-    - KEEP THESE KEYS SECRET!!!
-
-2. Install ask-cli
+## Usage
 
 ```
-npm install -g ask-cli
+$ git clone https://github.com/kun432/alexa-ask-cli-sample.git
+$ cd alexa-ask-cli-sample
+$ cd HelloWorldJP
+$ ask deplpy
 ```
 
-git clone https://github.com/kun432/alexa-ask-cli-sample.git
-cd alexa-ask-cli-sample
+Maybe...
